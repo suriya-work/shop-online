084168af04248a00d469b9670b58462469daf372
suriya babaei <suriyalifework60@gmail.com>
2024-01-29 14:21:41 +0330

    style hero section

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -1,10 +1,13 @@
 import Card from '../card/Card';
-import fashion from '../../../public/images/hero.jpg'
+import fashion from '../../../public/images/fashion.png'
 import Box from '../box/Box';
 const HeroSection = () => {
     return (
         <>
-            <div className='container grid sm:grid-cols-2 lg:grid-cols-2 mt-[50px]'>
+            <div className='container '>
+                <img src={fashion} alt="fashion" className=' flex justify-end' />
+            </div>
+            {/* <div className='container grid sm:grid-cols-2 lg:grid-cols-2 mt-[50px]'>
                 <div className='flex flex-col  justify-center items-center xl:items-start'>
                     <h1 className='text-black font-bold text-[30px] text-center lg:text-start'>Find the New Winter<br /> Trending Clothes In <br />The World</h1>
                     <p className='text-[14px] text-myRed font-medium text-center lg:text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br /> Deleniti blanditiis possimus sit ipsam voluptate iste<br /> exercitationem fugit ipsa nihil voluptatem totam.</p>
@@ -15,7 +18,7 @@ const HeroSection = () => {
                 <div className='flex justify-end'>
                     <img src={fashion} alt="fashion" className=' flex justify-end' />
                 </div>
-            </div>
+            </div> */}
             <Box />
             <Card titleName="Hot Products men's Clothing" firstName="men's clothing" />
             <Card titleName="Hot Products Jewelery" firstName="jewelery" />
