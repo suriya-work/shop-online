c974436f70e2b30f41fbf85430be8cb9854c0e5a
suriya babaei <suriyalifework60@gmail.com>
2024-02-11 10:45:47 +0330

    change cartmodal

--- src/components/cartmodal/Cartmodal.jsx
+++ src/components/cartmodal/Cartmodal.jsx
@@ -8,24 +8,31 @@ const Cartmodal = ({ cart, shown, close }) => {
   return shown ? (
     <div className="modal-backdrop" onClick={close}>
       <div
-        className="bg-white md:top-[7%] md:w-[95%] lg:w-[40%] w-[95%] top-[8%] h-[500px]  shadow-2xl border  fixed z-30 right-[0px] lg:top-[11%] overflow-y-scroll scrollbar-hide"
+        className="bg-white md:top-[7%] md:w-[95%] lg:w-[40%] w-[95%] top-[8%] h-[500px]  shadow-2xl border  fixed z-30 right-[0px] lg:top-[12.5%] overflow-y-scroll scrollbar-hide"
         onClick={(e) => e.stopPropagation()}
       >
         {cart.length === 0 ? (
           <div className="h-full flex justify-center items-center text-center  text-2xl">
-            <h2 className="text-center m-auto text-black">Your Cart Is Empty!</h2>
+            <h2 className="text-center m-auto text-black">
+              Your Cart Is Empty!
+            </h2>
           </div>
         ) : (
           cart.slice(0, 3).map((search) => {
             return (
-              <div key={search.id} className="flex items-center my-10 hover:shadow-sm pb-1 cursor-pointer pr-2">
+              <div
+                key={search.id}
+                className="flex items-center my-10 hover:shadow-sm pb-1 cursor-pointer pr-2"
+              >
                 <div className="w-[90px] h-[70px] mx-5">
                   <Link to={`/products/${search.id}`}>
                     <img src={search.image} alt="" className="w-full h-full" />
                   </Link>
                 </div>
                 <div className=" w-[80%] flex flex-col gap-4">
-                  <p className="text-black">{search.title.substring(0, 35)}...</p>
+                  <p className="text-black">
+                    {search.title.substring(0, 35)}...
+                  </p>
                   <div className="flex items-center gap-4">
                     <p className="text-primery font-bold mt-1 ">
                       ${search.price}
