21634e3f3fe8228e08abfbd2424c07b33b65b76c
suriya babaei <suriyalifework60@gmail.com>
2024-02-12 17:05:18 +0330

    change Title

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-
 const Title = ({ children }) => {
-    return (
-        <div className="my-10 flex justify-center items-center flex-col">
-            <h1 className="md:text-[2rem] text-[1rem] ">{children}</h1>
-            <div className="flex flex-col">
-                <div className="w-[80px] h-[2px] bg-myRed mt-1 ml-2"></div>
-                <div className="w-[80px] h-[2px] bg-myRed mt-2"></div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="my-5 flex justify-center items-center flex-col">
+      <h1 className="md:text-[2rem] text-[1rem] ">{children}</h1>
+      <div className="flex flex-col">
+        <div className="w-[80px] h-[2px] bg-myRed mt-1 ml-2"></div>
+        <div className="w-[80px] h-[2px] bg-myRed mt-2"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Title
+export default Title;
