8b2cee7633b8bee91c715e19d9e9fce56a9a281f
suriya babaei <suriyalifework60@gmail.com>
2024-02-17 18:07:41 +0330

    add data category

---
+++ src/data/categoryItemsList.jsx
@@ -0,0 +1,40 @@
+export const categoryItemsList = [
+    {
+      title: "SHOE & BAG",
+      list: ["Casual shoes", "boots", "sandals", "slippers"],
+    },
+    {
+      title: "Women Clothes",
+      list: ["Casual shoes", "boots", "sandals", "slippers"],
+    },
+    {
+      title: "SHIRTS",
+      list: ["Casual shoes", "boots", "sandals", "slippers"],
+    },
+    {
+      title: "ACCESORRES",
+      list: [
+        "Casual shoes",
+        "boots",
+        "sandals",
+        "slippers",
+        "boots",
+        "sandals",
+        "slippers",
+        "boots",
+        "sandals",
+      ],
+    },
+    {
+      title: "JEWERLLY",
+      list: ["Casual shoes", "boots", "sandals", "slippers"],
+    },
+    {
+      title: "SHOE & BAG",
+      list: ["Casual shoes", "boots", "sandals", "slippers"],
+    },
+    {
+      title: "SHOE & BAG",
+      list: ["Casual shoes", "boots", "sandals", "slippers"],
+    },
+  ];
