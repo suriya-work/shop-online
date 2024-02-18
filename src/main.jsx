4b2cc5006a73b86b44a2c8d64ea5a92d4b092ed6
suriya babaei <suriyalifework60@gmail.com>
2024-02-18 18:51:03 +0330

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/Store";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
 )
