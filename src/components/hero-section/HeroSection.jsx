4ae35e1e79be71d11312243797958c5a2606b7d2
suriya babaei <suriyalifework60@gmail.com>
2024-02-21 12:06:06 +0330

    import box on hero section

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -1,14 +1,14 @@
 import Card from '../card/Card';
 import fashion from '/images/top.png'
 import Cards from '../card/Cards';
-// import Box from '../box/Box';
+import Box from '../box/Box';
 const HeroSection = () => {
     return (
         <>
             <div className='w-full'>
                 <img src={fashion} alt="fashion"  className='w-full'/>
             </div>
-            {/* <Box /> */}
+            <Box />
             <Card titleName="Hot Products men's Clothing" firstName="men's clothing" />
             <Card titleName="Hot Products Jewelery" firstName="jewelery" />
             <Card titleName="Hot Products Electronics" firstName="electronics" />
