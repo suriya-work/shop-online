93bbf500083d4ca0674c965e539b97b4f5ff109b
suriya babaei <suriyalifework60@gmail.com>
2024-02-21 12:05:49 +0330

    add box on page home

--- src/components/box/Box.jsx
+++ src/components/box/Box.jsx
@@ -1,13 +1,13 @@
 import { BoxImage } from '../api/box'
 const Box = () => {
     return (
-        <div className='container mx-uto  grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-5 my-12'>
+        <div className='container hidden md:grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-5 my-12'>
             {
                 BoxImage.map((box) => {
                     return (
                         <div key={box.id}>
-                            <img className='w-[48px] h-[48px] m-auto' src={box.image} alt="" />
-                            <p className='text-[#000] text-center text-[16px] font-bold mt-7'>{box.title}</p>
+                            <img className='w-[40px] h-[40px] m-auto' src={box.image} alt="" />
+                            <p className='text-[#000] text-center text-[14px]  mt-5'>{box.title}</p>
                         </div>
                     )
                 })
