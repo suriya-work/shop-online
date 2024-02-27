eebf7df26b04f59dd819c523e8f949a20b8e9ba8
suriya babaei <suriyalifework60@gmail.com>
2024-02-27 14:14:11 +0330

    add components brand logo

---
+++ src/components/brand/BrandLogo.jsx
@@ -0,0 +1,36 @@
+const BrandLogoItems = [
+  {
+    id: 1,
+    image: "/images/brand-2.png",
+  },
+  {
+    id: 2,
+    image: "/images/brand-3.png",
+  },
+  {
+    id: 3,
+    image: "/images/brand-4.png",
+  },
+  {
+    id: 4,
+    image: "/images/01.png",
+  },
+  {
+    id: 5,
+    image: "/images/05.png",
+  },
+];
+
+const BrandLogo = () => {
+  return (
+    <div className="container grid grid-cols-2 md:grid-cols-5 justify-items-center place-items-center my-10 gap-5">
+      {BrandLogoItems.map((item) => (
+        <div key={item.id}>
+          <img src={item.image} alt="brand" width={130} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BrandLogo;
