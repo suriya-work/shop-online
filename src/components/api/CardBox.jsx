5c30f17f9cd746b4ed48e53ec9bf7551c488aa8d
suriya babaei <suriyalifework60@gmail.com>
2024-02-29 14:35:35 +0330

    add data card box

---
+++ src/components/api/CardBox.jsx
@@ -0,0 +1,31 @@
+export const CardBox = [
+    {
+        id:1,
+        image: '/images/card-1.png',
+    },
+    {
+        id:2,
+        image: '/images/card-2.png',
+
+    },
+    {
+        id:3,
+        image: '/images/card-3.png',
+
+    },
+    {
+        id:4,
+        image: '/images/card-4.png',
+
+    },
+    {
+        id:5,
+        image: '/images/card-5.png',
+
+    },
+    {
+        id:6,
+        image: '/images/card-6.png',
+
+    },
+]
