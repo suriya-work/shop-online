7b633f37df7da56c3c91336b8fc2f63ee7d3af4c
suriya babaei <suriyalifework60@gmail.com>
2024-02-29 14:36:20 +0330

    change hero section

--- src/components/card/Newproducts.jsx
+++ src/components/card/Newproducts.jsx
@@ -1,80 +1,86 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchAllProducts } from '../../redux/features/products/productSlice';
-import Title from '../title/Title';
-import { IoIosStarOutline } from 'react-icons/io';
-import { addToCart } from '../../redux/features/products/productSlice';
-import { Link } from 'react-router-dom';
-import Box from '../box/Box';
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAllProducts } from "../../redux/features/products/productSlice";
+import { Link } from "react-router-dom";
+import AddToCartBtn from "../addtocartbtn/AddToCartBtn";
+import { MdFavoriteBorder } from "react-icons/md";
+import { FaStar } from "react-icons/fa";
+import { useEffect } from "react";
 
 const Newproducts = () => {
   const dispatch = useDispatch();
-  const carts = useSelector((state) => state.product.products)
+  const carts = useSelector((state) => state.product.products);
   useEffect(() => {
-    dispatch(fetchAllProducts())
-  }, [])
-  const cartMyData = carts.filter((items) => items.category === "men's clothing")
+    dispatch(fetchAllProducts());
+  }, []);
+  const cartMyData = carts.filter((items) => items.category === "jewelery");
 
   return (
     <>
-      <Box />
-
-      <div className='container mx-auto mb-20'>
-        <div className='text-center'>
-          <Title>Hot New Products</Title>
-        </div>
-        <div className='grid grid-cols sm:grid-cols md:grid-cols-2  lg:grid-cols-4  justify-center gap-7 cartItems'>
-          {
-            cartMyData.map((item) => {
+      <div className="">
+        <div className="container my-10 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center">
+          {cartMyData
+            // .filter((cat) => cat.category === props.filterName)
+            // .slice(0, 4)
+            .map((item) => {
               return (
-                <div key={item.id} className='border rounded-[30px] overflow-hidden w-[300px] h-[550px] p-5 hover:shadow-2xl cursor-pointer'>
+                <section
+                  className=" flex flex-col justify-around cursor-pointer  transition duration-500 "
+                  key={item.id}
+                >
                   <Link to={`/products/${item.id}`}>
-                    <img src={item.image} alt="api" className='w-[100%] h-[50%]' />
+                    <img
+                      src={item.image}
+                      alt="aks"
+                      className="w-[100px] h-[100px] overflow-hidden mt-5 transition duration-300 ease-in-out hover:scale-110 m-auto"
+                    />
                   </Link>
-                  <div className='flex justify-center pt-5'>
-                    <IoIosStarOutline size={14} color='#A71B4A' />
-                    <IoIosStarOutline size={14} color='#A71B4A' />
-                    <IoIosStarOutline size={14} color='#A71B4A' />
-                    <IoIosStarOutline size={14} color='#A71B4A' />
-                    <IoIosStarOutline size={14} color='#A71B4A' />
-                  </div>
-                  <p className='text-center text-[15px] text-black pt-7 truncate'>{item.title}</p>
-                  <p className='text-center text-[14px] text-black pt-7'>{item.category}</p>
-                  <div className=" text-center mb-10">
-                    <p
-                      className="text-[#A71B4A] text-[16px] font-bold mt-7"
-                    // style={{
-                    //     display: isActive ? "none" : "",
-                    // }}
-                    >
-                      ${item.price}
-                    </p>
-                    <button title='Add To Cart'
-                      className="border text-black rounded-[20px] w-[150px] h-[40px] pt-1 mt-6 pb-1 font-bold hover:bg-myRed hover:text-white"
-                      // style={{
-                      //     display: isActive ? "flex" : "",
-                      // }}
-                      onClick={() => dispatch(addToCart({
-                        id: item.id,
-                        title: item.title,
-                        image: item.image,
-                        price: item.price,
-                      }))}
-                    >
+                  {/* first section */}
+                  <div className="pl-5 flex justify-between h-[40%]">
+                    {/* <div className="flex flex-col justify-around">
+                      <p className=" text-sm font-semibold" title={item.title}>
+                        {item.title.substring(0, 20)}...
+                      </p>
 
-                      add to cart
-                    </button>
-                  </div>
-                </div>
-              )
-            })
-          }
+                      <div className="flex gap-5 ">
+                        <span className="flex">
+                          <FaStar size={16} color="gold" />
+                          <FaStar size={16} color="gold" />
+                          <FaStar size={16} color="gold" />
+                          <FaStar size={16} color="gold" />
+                          <FaStar size={16} color="gold" />
+                        </span>
+                        <span className="text-sm text-gray-500">
+                          ({item.rating.count})
+                        </span>
+                      </div>
 
+                      <p className="text-red-500 text-sm font-bold mb-3 ">
+                        ${item.price}
+                      </p>
+                    </div> */}
+
+                    {/* second section */}
+
+                    {/* <div className="flex flex-col items-center mt-3 justify-around pr-5">
+                      <MdFavoriteBorder
+                        size={21}
+                        className="hover:text-red-500 mb-8"
+                      />
+                      <AddToCartBtn
+                        item={item}
+                        className={
+                          "border bg-white text-black rounded-md p-2  justify-center items-center  hover:bg-primery hover:text-white transition-all ease-linear duration-300"
+                        }
+                      />
+                    </div> */}
+                  </div>
+                </section>
+              );
+            })}
         </div>
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default Newproducts
+export default Newproducts;
--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -1,22 +1,47 @@
-import Card from '../card/Card';
-import fashion from '/images/top.png'
+import Card from "../card/Card";
+import fashion from "/images/image-product.png";
 import Cards from '../card/Cards';
-import Box from '../box/Box';
-import BrandLogo from '../brand/BrandLogo';
+// import Box from '../box/Box';
+import BrandLogo from "../brand/BrandLogo";
 const HeroSection = () => {
-    return (
-        <>
-            <div className='w-full'>
-                <img src={fashion} alt="fashion"  className='w-full'/>
-            </div>
-            <Box />
-            <Card titleName="Hot Products men's Clothing" firstName="men's clothing" />
-            <Card titleName="Hot Products Jewelery" firstName="jewelery" />
-            <Card titleName="Hot Products Electronics" firstName="electronics" />
-            <BrandLogo />
-            <Cards />
-        </>
-    )
-}
+  return (
+    <>
+      <div className="container grid  lg:grid-cols-3 grid-rows-1 gap-4 items-center py-10">
+        <div className="flex flex-col lg:items-start lg:text-start items-center text-center">
+          <h1 className="text-black font-semibold lg:text-4xl pb-8">
+            Collections
+          </h1>
+          <p className="text-gray-800 font-normal text-xl leading-7">
+            you can explore ans shop many differnt collection from various
+            barands here.
+          </p>
+          <div className="py-10">
+            <button className="text-white bg-primery w-40 h-10 rounded-md text-center">
+              Shop Now
+            </button>
+          </div>
+        </div>
+        <div className="col-span-2 ml-auto">
+          <img
+            src={fashion}
+            alt="fashion"
+            width={400}
+            // height={200}
+            className=""
+          />
+        </div>
+      </div>
+      <BrandLogo />
+      {/* <Box /> */}
+      <Card
+        titleName="Hot Products men's Clothing"
+        firstName="men's clothing"
+      />
+      <Card titleName="Hot Products Jewelery" firstName="jewelery" />
+      <Card titleName="Hot Products Electronics" firstName="electronics" />
+      <Cards />
+    </>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
