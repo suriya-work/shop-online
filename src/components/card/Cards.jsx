797f39f257ef0bec24f901bbb7eafb223412c092
suriya babaei <suriyalifework60@gmail.com>
2024-06-16 13:20:37 +0330

    add

--- src/components/card/Cards.jsx
+++ src/components/card/Cards.jsx
@@ -7,7 +7,7 @@ const Cards = () => {
           Follow products and discounts on Instagram
         </h2>
       </div>
-      <div className=" grid grid-cols-3 lg:grid-cols-6 items-center gap-5">
+      <div className="grid grid-cols-3 lg:grid-cols-6 items-center gap-5">
         {CardBox.map((item) => (
           <img key={item.id} src={item.image} alt="image" className=" rounded-md" />
         ))}
