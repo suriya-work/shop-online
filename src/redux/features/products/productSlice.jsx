5201bce350f8303a106b21f8b1df96055ac6480a
suriya babaei <suriyalifework60@gmail.com>
2024-06-18 11:36:19 +0330

    change redux

--- src/redux/features/products/productSlice.jsx
+++ src/redux/features/products/productSlice.jsx
@@ -1,75 +1,111 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    isLoading: false,
-    products: [],
-    cart: [],
-    isError: false,
-    amount: 0,
-    total: 0
-}
+  isLoading: false,
+  products: [],
+  cart: [],
+  favorite: [],
+  isError: false,
+  amount: 0,
+  total: 0,
+};
 export const fetchAllProducts = createAsyncThunk(
-    "productSlice/fetchAllProducts",
-    async () => {
-        const res = await axios.get('https://fakestoreapi.com/products');
-        // const res = await axios.get('https://dummyjson.com/products');
-        return res.data;
-    }
-)
+  "productSlice/fetchAllProducts",
+  async () => {
+    const res = await axios.get("https://fakestoreapi.com/products");
+    // const res = await axios.get('https://dummyjson.com/products');
+    return res.data;
+  }
+);
 
 const productSlice = createSlice({
-    name: 'productSlice',
-    initialState,
-    reducers: {
-        addToCart: (state, action) => {
-            const addToCartData = state.cart.find((item) => item.id === action.payload.id)
-            addToCartData ? addToCartData.quantity++ : state.cart.push({ ...action.payload, quantity: 1 });
-        },
-        incrementQuantity: (state, action) => {
-            const item = state.cart.find((item) => item.id === action.payload)
-            item.quantity++;
-        },
-        decrementQuantity: (state, action) => {
-            const item = state.cart.find((item) => item.id === action.payload)
-            item.quantity === 1 ? (item.quantity = 1) : item.quantity--;
-        },
-        removeItem: (state, action) => {
-            const deleteItem = state.cart.filter((item) => item.id !== action.payload)
-            state.cart = deleteItem;
-        },
-        updateTotal: (state) => {
-            let amount = 0;
-            let total = 0;
-            state.cart.map((item) => {
-              amount += item.quantity;
-              total += item.quantity * item.price;
-            });
-            state.total = total;
-            state.amount = amount;
-          },
-          removeCartItem : (state) => {
-            state.cart= []
-          },
+  name: "productSlice",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const addToCartData = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+      addToCartData
+        ? addToCartData.quantity++
+        : state.cart.push({ ...action.payload, quantity: 1 });
     },
-    extraReducers: (bulider => {
-        bulider.addCase(fetchAllProducts.fulfilled, (state, action) => {
-            state.products = action.payload
-            state.isLoading = false
-        })
+    addToFavorite: (state, action) => {
+      const addToFavoriteData = state.favorite.find(
+        (item) => item.id === action.payload.id
+      );
 
-        bulider.addCase(fetchAllProducts.pending, (state) => {
-            state.isLoading = true
-        })
-        bulider.addCase(fetchAllProducts.rejected, (state) => {
-            state.isLoading = false
-            state.products = []
-            state.isError = true
-        })
-    })
+      if (addToFavoriteData) {
+        addToFavoriteData.quantity++;
+      } else {
+        state.favorite.push({ ...action.payload, quantity: 1 });
+      }
+    },
 
+    addToFavorite: (state, action) => {
+      const addToFavoriteData = state.favorite.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (addToFavoriteData) {
+        addToFavoriteData.quantity++;
+      } else {
+        state.favorite.push({ ...action.payload, quantity: 1 });
+      }
+    },
+
+    incrementQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload);
+      item.quantity++;
+    },
+    decrementQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload);
+      item.quantity === 1 ? (item.quantity = 1) : item.quantity--;
+    },
+    removeItem: (state, action) => {
+      const deleteItem = state.cart.filter(
+        (item) => item.id !== action.payload
+      );
+      state.cart = deleteItem;
+    },
+    updateTotal: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cart.map((item) => {
+        amount += item.quantity;
+        total += item.quantity * item.price;
+      });
+      state.total = total;
+      state.amount = amount;
+    },
+    removeCartItem: (state) => {
+      state.cart = [];
+    },
+  },
+  extraReducers: (bulider) => {
+    bulider.addCase(fetchAllProducts.fulfilled, (state, action) => {
+      state.products = action.payload;
+      state.isLoading = false;
+    });
+
+    bulider.addCase(fetchAllProducts.pending, (state) => {
+      state.isLoading = true;
+    });
+    bulider.addCase(fetchAllProducts.rejected, (state) => {
+      state.isLoading = false;
+      state.products = [];
+      state.isError = true;
+    });
+  },
 });
 
-
-export const { addToCart, incrementQuantity, decrementQuantity, removeItem , updateTotal , removeCartItem} = productSlice.actions;
-export default productSlice.reducer
+export const {
+  addToCart,
+  incrementQuantity,
+  decrementQuantity,
+  removeItem,
+  updateTotal,
+  removeCartItem,
+} = productSlice.actions;
+export default productSlice.reducer;
